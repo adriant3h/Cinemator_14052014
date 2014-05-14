c8c95c51895fc989aca7018b6b71f8e77aaa9068
Adrian Teh <adrianteh@hotmail.com>
2014-05-14 16:15:46 +1000

    added copy to xampp batch script

---
+++ copy_to_xampp.bat
@@ -0,0 +1,3 @@
+set source=C:\Users\adrian.teh.POTENTIATE\Documents\GitHub\Cinemator_14052014\laravel
+set destination=C:\xampp\htdocs\Cinemator_14052014\laravel
+xcopy %source% %destination% /y /s /i
